7b51b4d708e3673c3c2378087cbcc69b748157e4
rmiyoyo <miyoyoraphael@gmail.com>
2023-12-16 23:50:42 +0300

    Add controller specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,39 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe UsersController, type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get '/users'
+      expect(response.body).to include('A list of all users')
+    end
+  end
+
+  describe 'GET #show' do
+    let(:user) { User.create(name: 'John Doe', photo: nil, bio: nil, posts_counter: 0) }
+
+    it 'returns a successful response' do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get "/users/#{user.id}"
+      expect(response.body).to include('Details for a given user')
+    end
   end
 end
