8930bbcde6ecb1bb09b1950f05ecf1c53741faf1
rmiyoyo <miyoyoraphael@gmail.com>
2024-01-14 01:07:54 +0300

    Update schema

--- Gemfile
+++ Gemfile
@@ -81,3 +81,5 @@ gem 'rails-controller-testing'
 gem 'tailwindcss-rails', '~> 2.0'
 
 gem 'devise', '~> 4.8', '>= 4.8.1'
+
+gem 'cancancan'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_14_214137) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_13_203226) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_14_214137) do
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    # added this below
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -64,7 +63,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_14_214137) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
